386b2bd2459898113504015643b83dd66ef860e7
newincpp <newincpp@gmail.com>
2015-05-03 01:01:11 +0900

    problem seems fixed

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main(void){
     GRand::Material m;
     e->queueIntruction([&m](){ m.addShader(GL_FRAGMENT_SHADER, "./shaders/defaultFragment.glsl"); });
     e->queueIntruction([&m](){ m.addShader(GL_VERTEX_SHADER, "./shaders/defaultVertex.glsl"); });
-    e->queueIntruction([&m](){ m.compileAll(); });
     e->queueIntruction([&m](){ m.link(); });
 
     sleep(5);
--- material.cpp
+++ material.cpp
@@ -10,23 +10,27 @@ namespace GRand {
 	    private:
 		GLuint _shaderId;
 		bool _compiled;
-		inline const char* getStringFromFile(const std::string& filename) const noexcept {
+		std::string _source;
+		inline void getStringFromFile(const std::string& filename) noexcept {
 		    std::ifstream t(filename);
-		    const std::string _t((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-		    return _t.c_str();
+		    const std::string s((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+		    _source = s;
 		}
 		inline bool _checkCompile()const noexcept {
 		    GLint compileStatus;
 		    glGetShaderiv(_shaderId, GL_COMPILE_STATUS, &compileStatus);
 		    if (compileStatus == GL_TRUE) {
-			std::cout << "\033[1;32mcompilation done with success\033[0m" << std::endl;
+			std::cout << "\033[0;32msuccesfully compiled\033[0m" << std::endl;
 			return true;
 		    }
 		    GLint InfoLogLength;
 		    glGetShaderiv(_shaderId, GL_INFO_LOG_LENGTH, &InfoLogLength);
 		    char ErrorMessage[InfoLogLength];
 		    glGetShaderInfoLog(_shaderId, InfoLogLength, NULL, ErrorMessage);
-		    std::cout << "\033[1;31mfailed to compile shader, error log: " << std::endl << ErrorMessage << std::endl << "-------------------\033[0m" << std::endl;
+		    std::cout << "\033[1;31mfailed to compile shader!" << std::endl <<
+			"your shader:" << std::endl << 
+			_source << std::endl <<
+			"error log:" << std::endl << ErrorMessage << std::endl << "-------------------\033[0m" << std::endl;
 		    return false;
 		}
 	    public:
@@ -41,23 +45,23 @@ namespace GRand {
 
 GRand::Shader::Shader(GLenum type, const std::string& filename) : _shaderId(0), _compiled(false) {
     _shaderId = glCreateShader(type);
-    const char* source = getStringFromFile(filename);
+    getStringFromFile(filename);
+    const char* source = _source.c_str();
     glShaderSource(_shaderId, 1, &source, NULL);
-    std::cout << "new id: " << _shaderId << " " << filename << std::endl;
+    std::cout << "compiling: " << filename << std::endl;
+    compile();
 }
 
-GRand::Shader::Shader(Shader&& s_) : _shaderId(s_._shaderId), _compiled(s_._compiled) {
+GRand::Shader::Shader(Shader&& s_) : _shaderId(s_._shaderId), _compiled(s_._compiled), _source(s_._source) {
 }
 
 void GRand::Shader::compile() noexcept {
-    std::cout << "compiling shader: " << _shaderId << std::endl;
     glCompileShader(_shaderId);
     _compiled = _checkCompile();
 }
 
 GRand::Shader::~Shader() {
     //std::cout << "deleting shader " << _shaderId << std::endl;
-    //glDeleteShader(_shaderId);
 }
 
 GRand::Material::Material() : _shaderProgram(0) {
@@ -67,38 +71,30 @@ void GRand::Material::addShader(GLenum type, const std::string& filename) noexce
     _shaders.emplace_back(type, filename);
 }
 
-void GRand::Material::compileAll() noexcept{
-    for (Shader& s : _shaders) {
-	s.compile();
-    }
-}
-
 void GRand::Material::link() noexcept {
-    std::cout << "linking shaders" << std::endl;
     if (!_shaderProgram) {
 	glDeleteProgram(_shaderProgram);
     }
     _shaderProgram = glCreateProgram();
     for (Shader& s : _shaders) {
 	if (!s.getCompileStatus()) {
-	    std::cout << "\033[1;33m warning: not compiled shader found" << std::endl;
+	    std::cout << "\033[1;33m warning: not compiled shader found\033[0m" << std::endl;
 	}
 	glAttachShader(_shaderProgram, s.getId());
     }
     glLinkProgram(_shaderProgram);
 
 
-    GLint compileStatus;
-    glGetShaderiv(_shaderProgram,  GL_LINK_STATUS, &compileStatus);
-    if (compileStatus == GL_TRUE) {
-	std::cout << "shader successfully compiled" << std::endl;
-	return;
+    GLint linkStatus;
+    glGetShaderiv(_shaderProgram,  GL_LINK_STATUS, &linkStatus);
+    if (linkStatus == GL_FALSE) {
+	GLint InfoLogLength;
+	glGetProgramiv(_shaderProgram, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	char ErrorMessage[InfoLogLength];
+	glGetProgramInfoLog(_shaderProgram, InfoLogLength, NULL, ErrorMessage);
+	std::cout << "\033[0;31mfailed to link error log: " << std::endl << ErrorMessage << std::endl << "-------------------\033[0m" << std::endl;
     }
-    GLint InfoLogLength;
-    glGetProgramiv(_shaderProgram, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    char ErrorMessage[InfoLogLength];
-    glGetProgramInfoLog(_shaderProgram, InfoLogLength, NULL, ErrorMessage);
-    std::cout << "\033[0;31mfailed to link error log: " << std::endl << ErrorMessage << std::endl << "-------------------\033[0m" << std::endl;
+    std::cout << "\033[0;32msuccessfully linked\033[0m" << std::endl;
 }
 
 void GRand::Material::use() const noexcept {
@@ -106,4 +102,7 @@ void GRand::Material::use() const noexcept {
 }
 
 GRand::Material::~Material() {
+    for (Shader& s: _shaders) {
+	glDeleteShader(s.getId());
+    }
 }
