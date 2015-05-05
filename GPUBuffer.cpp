f85a285268b21d65abe49093ff780c0603bbba42
koziar_c <koziar_c@epitech.eu>
2015-05-05 21:37:44 +0200

    problem solved =D

--- GPUBuffer.cpp
+++ GPUBuffer.cpp
@@ -6,21 +6,26 @@
 #include <fstream>
 #include "GPUBuffer.hh"
 
-GRand::GPUBuffer::GPUBuffer() : _vbo(0), _scene(NULL) {
+GRand::GPUBuffer::GPUBuffer() : _vbo(0), _ebo(0), _scene(NULL) {
 }
 
-GRand::GPUBuffer::GPUBuffer(const GPUBuffer& o_) : _vbo(o_._vbo) {
+GRand::GPUBuffer::GPUBuffer(const GPUBuffer& o_) : _vbo(o_._vbo), _ebo(o_._ebo) {
     if (_vbo == 0) {
 	_vertexArray = o_._vertexArray;
+    }
+    if (_ebo == 0) {
+	_elementArray = o_._elementArray;
     }
 }
 
 void GRand::GPUBuffer::operator=(const GPUBuffer& o_) {
     _vbo = o_._vbo;
+    _ebo = o_._ebo;
     if (_vbo) {
 	return;
     }
     _vertexArray = o_._vertexArray;
+    _elementArray = o_._elementArray;
 }
 
 bool GRand::GPUBuffer::loadFile(std::string const &name) {
@@ -39,7 +44,6 @@ bool GRand::GPUBuffer::loadFile(std::string const &name) {
 	return false;
     }
     getAllFaces(_scene, _scene->mRootNode);
-    std::cout << "ea size once file loaded:" << _elementArray.size() << std::endl;
     return true;
 }
 
@@ -48,8 +52,6 @@ void GRand::GPUBuffer::getAllFaces(const struct aiScene *sc, const struct aiNode
 
     for (; n < nd->mNumMeshes; ++n) {
 	const struct aiMesh* mesh = sc->mMeshes[nd->mMeshes[n]];
-	unsigned int *faceArray;
-	faceArray = (unsigned int *)malloc(sizeof(unsigned int) * mesh->mNumFaces * 3);
 	if (_vertexArray.size() + mesh->mNumFaces * 6 > _vertexArray.capacity()) {
 	    _vertexArray.reserve(_vertexArray.capacity() + mesh->mNumFaces * 6);
 	}
@@ -58,13 +60,13 @@ void GRand::GPUBuffer::getAllFaces(const struct aiScene *sc, const struct aiNode
 	    _vertexArray.push_back(mesh->mVertices[i].x);
 	    _vertexArray.push_back(mesh->mVertices[i].y);
 	    _vertexArray.push_back(mesh->mVertices[i].z);
-	    //_vertexArray.push_back(mesh->mNormals[i].x);
-	    //_vertexArray.push_back(mesh->mNormals[i].y);
-	    //_vertexArray.push_back(mesh->mNormals[i].z);
+	    _vertexArray.push_back(mesh->mNormals[i].x);
+	    _vertexArray.push_back(mesh->mNormals[i].y);
+	    _vertexArray.push_back(mesh->mNormals[i].z);
 	}
-	//if (_elementArray.size() + mesh->mNumFaces > _elementArray.capacity()) {
-	//    _elementArray.reserve(_elementArray.capacity() + mesh->mNumFaces);
-	//}
+	if (_elementArray.size() + mesh->mNumFaces > _elementArray.capacity()) {
+	    _elementArray.reserve(_elementArray.capacity() + mesh->mNumFaces);
+	}
 	for (unsigned int t = 0; t < mesh->mNumFaces; ++t) {
 	    const struct aiFace* face = &mesh->mFaces[t];
 	    for (unsigned int i = 0; i < face->mNumIndices; i++) { // go through all vertices in face
@@ -79,43 +81,17 @@ void GRand::GPUBuffer::getAllFaces(const struct aiScene *sc, const struct aiNode
 }
 
 void GRand::GPUBuffer::generateVBOAndVertexArray() {
-    /*GLfloat vertices[] = {
-	-0.5f,  0.5f, 0.f, // Top-left
-	0.5f,  0.5f, 0.f, // Top-right
-	0.5f, -0.5f, 0.f, // Bottom-right
-	-0.5f, -0.5f, 0.f // Bottom-left
-    };
-    GLuint elements[] = {
-    0, 1, 2, 18, 0, 2, 3, 4, 5, 19, 3, 5, 6, 7, 8, 20, 6, 8, 9, 10, 11, 21, 9, 11, 12, 13, 14, 22, 12, 14, 15, 16, 17, 23, 15, 17
-    };*/
     if (_vbo) {
 	glDeleteBuffers(1, &_vbo);
     }
-    std::cout << "ea size on generate ebo:" << _elementArray.size() << std::endl;
-    //_vertexArray = b_;
     glGenBuffers(1, &_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, _vbo);
     glBufferData(GL_ARRAY_BUFFER, _vertexArray.size() * sizeof(decltype(_vertexArray)::value_type), &(_vertexArray[0]), GL_STATIC_DRAW);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &_ebo);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, _elementArray.size() * sizeof(decltype(_vertexArray)::value_type), &(_elementArray[0]), GL_STATIC_DRAW);
-    std::cout << "ELEMENT ARRAY:";
-    for (std::vector<GLuint>::iterator it = _elementArray.begin(); it != _elementArray.end(); it++) {
-	std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-    /*int i = 0;
-    for (std::vector<GLfloat>::iterator it = _vertexArray.begin(); it != _vertexArray.end(); it++) {
-	i++;
-	std::cout << *it << " ";
-	if (i % 3 == 0) {
-	    std::cout << std::endl;
-	}
-    }*/
 }
 
 void GRand::GPUBuffer::setBuffer(const decltype(_vertexArray)& b_) {
@@ -136,11 +112,13 @@ void GRand::GPUBuffer::draw(GLenum drawStyle_) const noexcept {
     glEnableVertexAttribArray(1); // enable normal shader parameter value
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
     glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(decltype(_vertexArray)::value_type), (void*)0);
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(decltype(_vertexArray)::value_type), (void*)0);
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(decltype(_vertexArray)::value_type), (void*)0);
 
     // when normal enabled
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(decltype(_vertexArray)::value_type), (void*)0); // vertex
-    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(decltype(_vertexArray)::value_type), 3 * sizeof(decltype(_vertexArray)::value_type)); //normal
+     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(decltype(_vertexArray)::value_type), (void*)0); // vertex
+     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(decltype(_vertexArray)::value_type), (void*)0); // vertex
+     //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(decltype(_vertexArray)::value_type), (void*)3 * sizeof(decltype(_vertexArray)::value_type)); //normal
 
     // with vertex color enabled
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(decltype(_vertexArray)::value_type), (void*)0); // vertex
@@ -149,10 +127,7 @@ void GRand::GPUBuffer::draw(GLenum drawStyle_) const noexcept {
 
     // draw the polygon with the shader on the OpenGL draw buffer
     //glDrawArrays(drawStyle_, 0, _vertexArray.size());
-    //std::cout << "ea size:" << _elementArray.size() << std::endl;
     glDrawElements(drawStyle_, _elementArray.size(), GL_UNSIGNED_INT, 0);
-    //glDrawElements(drawStyle_, 6, GL_UNSIGNED_INT, 0);
-    //glDrawArrays(drawStyle_, 0, 4);
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
 }
