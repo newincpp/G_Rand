d40fa63053d1d9619e690f5e2afb3708680ab26a
newincpp <newincpp@gmail.com>
2015-05-05 17:38:45 +0900

    add uniforms

---
+++ uniform.cpp
@@ -0,0 +1,63 @@
+﻿#include <eigen3/Eigen/Dense>
+#include "uniform.hh"
+
+//VECTORS
+template <>
+void Uniform<float>::upload() const {
+	glUniform1f(_location, _value);
+}
+template <>
+void Uniform<int>::upload() const {
+	glUniform1i(_location, _value);
+}
+template <>
+void Uniform<Eigen::Vector2f>::upload() const {
+	glUniform2f(_location, _value[0], _value[1]);
+}
+template <>
+void Uniform<Eigen::Vector3f>::upload() const {
+	glUniform3f(_location, _value[0], _value[1], _value[2]);
+}
+template <>
+void Uniform<Eigen::Vector4f>::upload() const {
+	glUniform4f(_location, _value[0], _value[1], _value[2], _value[3]);
+}
+
+//MATRIX
+
+template <>
+void Uniform<Eigen::Matrix2f>::upload() const {
+	glUniformMatrix2fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix3f>::upload() const {
+	glUniformMatrix3fv(_location, 1, GL_TRUE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix4f>::upload() const {
+	glUniformMatrix4fv(_location, 1, GL_TRUE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 2, 3> >::upload() const {
+	glUniformMatrix2x3fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 3, 2> >::upload() const {
+	glUniformMatrix3x2fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 2, 4> >::upload() const {
+	glUniformMatrix2x4fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 4, 2> >::upload() const {
+	glUniformMatrix4x2fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 3, 4> >::upload() const {
+	glUniformMatrix3x4fv(_location, 1, GL_FALSE, _value.data());
+}
+template <>
+void Uniform<Eigen::Matrix<float, 4, 3> >::upload() const {
+	glUniformMatrix4x3fv(_location, 1, GL_FALSE, _value.data());
+}
---
+++ uniform.hh
@@ -0,0 +1,64 @@
+﻿#ifndef UNIFORM_H_
+# define UNIFORM_H_
+#include <iostream>
+#include <string>
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+// Uniform class is a class that represent a Opengl's shader uniform values
+template <typename T>
+class Uniform {
+	private:
+		T _value;
+		GLint _location;
+	public:
+		// value is the default value, prgm the shader program, name the name of the value in the shader
+		//nb: except if you are doing very spetial stuff just call Graphics::getCurentShader() to get GLuint prgm parametter 
+		explicit Uniform(const T& value, GLuint prgm, const std::string& name) : _value(value), _location(glGetUniformLocation(prgm, name.c_str())) {
+			if (_location == -1) {
+				std::cout << "failed to bind location of uniform: \"" << name << "\"" << std::endl;
+			}
+		}
+		explicit Uniform() : _value(T()), _location(0) {}
+		explicit Uniform(const T& v_) : _value(v_), _location(0) {}
+		void relocate(GLuint prgm, const std::string& name) { _location = glGetUniformLocation(prgm, name.c_str()); }
+		inline GLint getLct() const noexcept { return _location; }
+		const Uniform<T>& operator=(const T& v) { _value = v; return *this; }
+		const Uniform<T>& operator*(const T& v) { _value = v; return *this; }
+		const Uniform<T>& operator/(const T& v) { _value = v; return *this; }
+		const Uniform<T>& operator+(const T& v) { _value = v; return *this; }
+		const Uniform<T>& operator-(const T& v) { _value = v; return *this; }
+		T operator+=(const T& v) { _value += v; return _value; }
+		T operator-=(const T& v) { _value -= v; return _value; }
+		T operator*=(const T& v) { _value *= v; return _value; }
+		T operator/=(const T& v) { _value /= v; return _value; }
+		const Uniform<T>& operator++() { ++_value; }
+		const Uniform<T>& operator--() { ++_value; }
+		bool operator==(const T& v) { return _value == v._value; }
+		bool operator!=(const T& v) { return _value != v._value; }
+		bool operator<(const T& v) { return _value < v._value; }
+		bool operator>(const T& v) { return _value > v._value; }
+		bool operator<=(const T& v) { return _value <= v._value; }
+		bool operator>=(const T& v) { return _value >= v._value; }
+		const Uniform<T>& operator=(const T&& v) { _value = v; return *this; }
+		const Uniform<T>& operator*(const T&& v) { _value = v; return *this; }
+		const Uniform<T>& operator/(const T&& v) { _value = v; return *this; }
+		const Uniform<T>& operator+(const T&& v) { _value = v; return *this; }
+		const Uniform<T>& operator-(const T&& v) { _value = v; return *this; }
+		void operator+=(const T&& v) { _value += v; }
+		void operator-=(const T&& v) { _value -= v; }
+		bool operator==(const T&& v) { return _value == v; }
+		bool operator!=(const T&& v) { return _value != v; }
+		bool operator<(const T&& v) { return _value < v; }
+		bool operator>(const T&& v) { return _value > v; }
+		bool operator<=(const T&& v) { return _value <= v; }
+		bool operator>=(const T&& v) { return _value >= v; }
+		T& operator>>(T&& v) { v = _value; return _value; }
+		// if you need a member of _value or your operator is not binded you need to call this function 
+		T& get() { return _value; }
+		// this function update uniform value on GPU  (so usually need to call it before drawing polygons)
+		virtual void upload() const;
+};
+
+#endif /* !UNIFORM_H_ */
