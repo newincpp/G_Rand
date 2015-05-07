fdfa6b5c49c212994ca6a6618cf30f10ae0cea21
_Nyu <newin@protonmail.ch>
2015-05-07 13:35:49 +0900

    fix after experiment in http://shdr.bkcore.com/ (need to be tested)

--- shaders/phongFrag.glsl
+++ shaders/phongFrag.glsl
@@ -1,36 +1,25 @@
 #version 440
 
 in vec3 fNormal;
-in mat4 cameraLoc;
 out vec4 outColor;
 
 void main() {
-    vec3 lightdir = vec3(1, 0, 1);
-
-    // material
-    vec4 diffuse = vec4(0, 0.3, 0.0, 1.0);
-    vec4 ambient = vec4(0.3, 0.3, 0.3, 1.0);
-    vec4 specular = vec4(0.5,0.5,0.5,0.9);
-    float shininess = 0.5;
-
-    vec3 eye = vec3(0,0,-2);
-
-
-    vec4 spec = vec4(0.0);
-
-    // normalize both input vectors
-    vec3 n = normalize(fNormal);
-    vec3 e = normalize(eye);
-
-    float intensity = max(dot(n,lightdir), 0.0);
-
-    if (intensity > 0.0) {
-	// compute the half vector
-	vec3 h = normalize(lightdir + e);  
-	// compute the specular term into spec
-	float intSpec = max(dot(h,n), 0.0);
-	spec = specular * pow(intSpec,shininess);
-    }
-    outColor = max(intensity *  diffuse + spec, ambient);
-    //outColor = vec4(0,0.3,0, 1.0);
+  vec4 spec = vec4(0.0);
+  vec3 lDir = vec3(1.0,1.0,.3);
+  vec3 eye = vec3(0.0, 0.0, 0.0);
+  
+  vec4 diffuse = vec4(0.8,0.8,0.8,1);
+  vec4 ambient = vec4(0.,0.,0.,1);
+  vec4 specular = vec4(0.1,0.1,0.1,1);
+  float shininess = 0.7;
+  
+  float intensity = max(dot(fNormal, lDir), 0.0);
+  
+  if (intensity > 0.0) {
+    vec3 h = normalize(lDir + eye);
+    float intSpec = max(dot(h,fNormal),0.0);
+    spec = specular * pow(intSpec, shininess);
+  }
+  0utColor = max(intensity * diffuse + spec, ambient);
+  //outColor = vec4(0,0.3,0, 1.0);
 }
