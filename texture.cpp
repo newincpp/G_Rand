16a633031818fa2c8e79486c6db891a9f0d4d1e2
newincpp <newincpp@gmail.com>
2015-06-20 17:29:25 +0900

    delete devil code to simplify

--- texture.cpp
+++ texture.cpp
@@ -26,7 +26,7 @@ void GRand::Texture::load() noexcept {
     glGenTextures(1, &_textureId);
     glBindTexture(GL_TEXTURE_2D, _textureId);
 
-
+    /*
     ilBindImage(_imgId);
     ilEnable(IL_ORIGIN_SET);
     ilOriginFunc(IL_ORIGIN_LOWER_LEFT);	
@@ -44,10 +44,16 @@ void GRand::Texture::load() noexcept {
     }
     //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
 
+    */
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    float pixels[] = {
+	0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
+    };
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
 
     //glGenerateMipmap(GL_TEXTURE_2D);
     _loaded = true;
